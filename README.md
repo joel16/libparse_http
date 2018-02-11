d8852c232be6ddaca6c1eb788afa11438aeb8bb7
Joel16 <sauskethunderkid@gmail.com>
2018-02-11 14:38:14 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # libparse_http
 Reverse engineered libparse_http.prx (6.61)
+
+This is a work in progress reverse of libparse_http.rx module for uOFW. You'll need uOFW to build this.
---
+++ libparse_http/libparse_http.c
@@ -0,0 +1,9 @@
+/* Copyright (C) 2011, 2012 The uOFW team
+   See the file COPYING for copying permission.
+*/
+
+#include <common_imp.h>
+
+SCE_MODULE_INFO("SceParseHTTPheader_Library", SCE_MODULE_ATTR_EXCLUSIVE_LOAD | SCE_MODULE_ATTR_EXCLUSIVE_START, 1, 1);
+SCE_SDK_VERSION(SDK_VERSION);
+
